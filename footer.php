0e2e52cf09445367e9aacbda664cc0f4d447caae
LAPTOP-NVUJBTUF\tomiz <tomizawa@efit.co.jp>
2023-10-19 17:54:23 +0900

    footer表記、とび先設定

--- footer.php
+++ footer.php
@@ -1,10 +1,13 @@
 <footer>
     <div class="f_inner">
         <ul>
-            <li><a href="#">HOME</a></li>
+            <li><a href="/">HOME</a></li>
             <li><a href="#">プライバシーポリシー</a></li>
-            <li><a href="#">お問い合わせ</a></li>
+            <li><a href="https://corporate.t-creative-works.com/contact/" target="_blank">お問い合わせ</a></li>
             <li><a href="#">Portfolio</a></li>
+        </ul>
+        <ul class="f_version">
+            <li><a href="#">Ver2.1.3</a></li>
         </ul>
         <div class="f_copy">copyright @tomizawa-creative-works</div>
     </div>
